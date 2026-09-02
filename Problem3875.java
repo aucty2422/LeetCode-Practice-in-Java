df2754008846dd72a8a4e7e0b9907c27d96fec59
Aucty <akhyatiwari22@gmail.com>
2026-09-02 19:52:38 +0530

    LeetCode Problem3875 Solution

---
+++ Problem3875.java
@@ -0,0 +1,20 @@
+/*
+Problem 3875: Construct Uniform Parity Array I
+
+Approach:
+1.. If all elements are already even, the condition is satisfied
+3. If odd elements are present, the even elements can be adjusted using the odd elements 
+4. Eventually all valid test cases only return true
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        
+        return true;
+
+    }
+}
+
